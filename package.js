ff1d2a6bb98a953cb7e283389df6aef954ac1882
Jon Lachlan <jonlachlan@gmail.com>
2015-11-06 10:42:00 -0600

    Create new forked package

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'trever:quilljs',
-  version: '0.0.4',
+  name: 'jonlachlan:quilljs',
+  version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'Quill.js version 0.20.0 for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/trever/quilljs',
+  git: 'https://github.com/jonlachlan/quilljs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,10 +12,11 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-
-  api.export('Quill', 'client');
-  api.addFiles('quill/quill.js', 'client');
-  api.addFiles('export.js', 'client');
+  // api.use('jonlachlan:ottypes-rich-text')
+  api.export('Quill');
+  api.addFiles('quill/quill.js');
+  api.addFiles('export.js');
   api.addFiles('quill/quill.base.css', 'client');
   api.addFiles('quill/quill.snow.css', 'client');
+  api.export('Delta');
 });
